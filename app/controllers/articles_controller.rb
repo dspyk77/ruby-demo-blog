6d41a51b68cc65bc10eb3ee493eb6ddde80d09d8
Daniel Spykstra <dspyk77@gmail.com>
2024-11-09 15:37:50 -0600

    7.3.2 use strong paramaters

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,29 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def article_params
+      params.expect(article: [:title, :body])
+    end
 end
+
+# redirect_to will cause the browser to make a new request,
+# whereas render renders the specified view for the current request.
+# It is important to use redirect_to after mutating the database or application state.
+# Otherwise, if the user refreshes the page, the browser will make the same request,
+# and the mutation will be repeated.
